27db994f268bf09200b88e72a0f19e858ef42860
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-01-24 00:50:12 +0800

    修改lectures文件夹的内容排布

--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -538,6 +538,7 @@ def run_load_checkpoint(
     """
     raise NotImplementedError
 
+from cs336_basics.tokenizer import Tokenizer
 
 def get_tokenizer(
     vocab: dict[int, bytes],
@@ -559,7 +560,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return Tokenizer(vocab = vocab, merges = merges, special_tokens = special_tokens)
 
 from cs336_basics.tokenizer import train_bpe
 
