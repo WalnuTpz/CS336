d2bb8d2b4288957f671590eba6b20cd7036db721
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-01-26 22:27:16 +0800

    完成linear类，通过测试

--- assignment1-basics/cs336_basics/layers.py
+++ assignment1-basics/cs336_basics/layers.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import math
 import torch
 import torch.nn as nn
+from torch import Tensor
+from einops import rearrange, einsum
 
 class Linear(nn.Module):
     def __init__(
@@ -13,8 +15,14 @@ class Linear(nn.Module):
         dtype: torch.dtype | None = None,
     ):
         super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
 
+        weight_tensor = torch.empty((out_features, in_features), device = device, dtype = dtype)
+        self.weight = nn.Parameter(weight_tensor)
+        std = math.sqrt(2.0 / (in_features + out_features))
+        nn.init.trunc_normal_(self.weight, mean=0.0, std=std, a=-3 * std, b=3 * std)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-
-        raise NotImplementedError
+    def forward(self, x: Tensor) -> Tensor:
+        y = einsum(self.weight, x, "d_out d_in, ... d_in -> ... d_out")
+        return y
