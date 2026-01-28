8f86134bb8e13bfe27bef2d28c29e869beb8a7fc
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-01-28 23:14:57 +0800

    完成AdamW类，通过测试

--- assignment1-basics/cs336_basics/optim.py
+++ assignment1-basics/cs336_basics/optim.py
@@ -3,6 +3,7 @@ from collections.abc import Callable
 from typing import Optional
 import math
 import torch
+from torch import Tensor
 
 class AdamW(torch.optim.Optimizer):
     def __init__(
@@ -13,8 +14,49 @@ class AdamW(torch.optim.Optimizer):
         eps: float = 1e-8,                 # ϵ
         weight_decay: float = 0.0,         # λ
     ):
-        super().__init__()
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay}
+        super().__init__(params, defaults)
 
-    @torch.no_grad()
+    @torch.no_grad()    # 默认不需要梯度跟踪
     def step(self, closure: Optional[Callable] = None):
-        raise NotImplementedError
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():    # 如果有 closure 函数，用它计算初始 loss
+                loss = closure()
+
+        for group in self.param_groups:    # 遍历参数组
+            # 加载对应的超参数
+            lr = group["lr"]
+            beta1, beta2 = group["betas"]
+            eps = group["eps"]
+            wd = group["weight_decay"]
+
+            for p in group["params"]:    # 遍历参数组内的每个张量 p
+                if p.grad is None:
+                    continue
+
+                g = p.grad.data
+                state = self.state[p]
+
+                # 如果是初始状态，则初始化 t, m, v
+                if len(state) == 0:
+                    state["t"]: int = 0
+                    state["m"]: Tensor = torch.zeros_like(p.data)
+                    state["v"]: Tensor = torch.zeros_like(p.data)
+
+                # 根据公式更新 t, m, v
+                t = state["t"] + 1
+                m = beta1 * state["m"] + (1 - beta1) * g
+                v = beta2 * state["v"] + (1 - beta2) * (g * g)
+
+                # 根据公式计算 alpha_t 并更新 p 的参数
+                alpha_t = lr * math.sqrt(1 - pow(beta2, t)) / (1 - pow(beta1, t))
+                p.data -= alpha_t * m / (v.sqrt() + eps)
+                p.data -= lr * wd * p.data
+
+                # 保存更新后的 t, m, v
+                state["t"] = t
+                state["m"] = m
+                state["v"] = v
+
+        return  loss
