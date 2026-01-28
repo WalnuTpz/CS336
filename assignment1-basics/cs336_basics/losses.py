57337b639718edffe1fa8cfd9f361c32e8120bd3
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-01-28 21:43:01 +0800

    完成perplexity，通过测试

--- assignment1-basics/cs336_basics/losses.py
+++ assignment1-basics/cs336_basics/losses.py
@@ -15,6 +15,9 @@ def cross_entropy(    # 计算交叉熵（这里没有直接使用原公式，�
     # 先取指数，再求和，最后取对数（这个函数不会数值溢出，所以不用将每个元素减去对应的最大元素以后再取指数）
     logit_y = logits_2d.gather(dim=-1, index=targets_1d[:, None]).squeeze(-1)
     # 根据 targets 取出 logits_2d 中对应的值，然后去掉最后一维
-    out = (log_denom - logit_y).mean()
+    out = (log_denom - logit_y).mean()  # (
 
     return out
+
+def perplexity(losses: Tensor) -> Tensor:    # 计算困惑度
+    return torch.exp(losses.mean())
