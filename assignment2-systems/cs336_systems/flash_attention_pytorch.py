058a06858ddbedac7771a67c35c07fbe85dd2411
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-03-08 19:01:31 +0800

    实现普通forward（未分块）

--- assignment2-systems/cs336_systems/flash_attention_pytorch.py
+++ assignment2-systems/cs336_systems/flash_attention_pytorch.py
@@ -1,6 +1,7 @@
 import math
 import torch
 from torch import Tensor
+from einops import einsum
 
 
 class FlashAttentionForwardPytorch(torch.autograd.Function):
@@ -12,7 +13,17 @@ class FlashAttentionForwardPytorch(torch.autograd.Function):
             V: Tensor,  # (B, H, N, D)
             is_causal: bool = False,
     ) -> Tensor:  # (B, H, N, D)
-        raise NotImplementedError
+        D = Q.shape[-1]
+        scale = 1.0 / math.sqrt(D)
+
+        S = einsum(Q, K, "... q d, ... k d -> ... q k") * scale  # (B, H, N, N)
+        L = torch.logsumexp(S, dim=-1)  # (B, H, N)
+        P = torch.softmax(S, dim=-1)  # (B, H, N, N)
+        O = einsum(P, V, "... q k, ... k d -> ... q d")  # (B, H, N, D)
+
+        ctx.save_for_backward(L, Q, K, V, O)
+        ctx.is_causal = is_causal
+        return O
 
     @staticmethod
     def backward(ctx, dO):
