1d873967eb34a03021ae233e95741fdad5b663d4
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-03-08 23:35:38 +0800

    使代码支持任意前导维度，通过测试

--- assignment2-systems/cs336_systems/flash_attention_pytorch.py
+++ assignment2-systems/cs336_systems/flash_attention_pytorch.py
@@ -8,12 +8,12 @@ class FlashAttentionForwardPytorch(torch.autograd.Function):
     @staticmethod
     def forward(
             ctx,
-            Q: Tensor,  # (B, H, N, D)
-            K: Tensor,  # (B, H, N, D)
-            V: Tensor,  # (B, H, N, D)
+            Q: Tensor,  # (..., N, D)
+            K: Tensor,  # (..., N, D)
+            V: Tensor,  # (..., N, D)
             is_causal: bool = False,
-    ) -> Tensor:  # (B, H, N, D)
-        B, H, N, D = Q.shape
+    ) -> Tensor:  # (..., N, D)
+        *batch_dims, N, D = Q.shape
         scale = 1.0 / math.sqrt(D)
         BLOCK_M = 16
         BLOCK_N = 16
@@ -23,29 +23,29 @@ class FlashAttentionForwardPytorch(torch.autograd.Function):
         Kf = K.float()
         Vf = V.float()
 
-        O = torch.zeros((B, H, N, D), device=Q.device, dtype=torch.float32)
-        m = torch.full((B, H, N), float("-inf"), device=Q.device, dtype=torch.float32)
-        l = torch.zeros((B, H, N), device=Q.device, dtype=torch.float32)
+        O = torch.zeros((*batch_dims, N, D), device=Q.device, dtype=torch.float32)
+        m = torch.full((*batch_dims, N), float("-inf"), device=Q.device, dtype=torch.float32)
+        l = torch.zeros((*batch_dims, N), device=Q.device, dtype=torch.float32)
 
         for i in range(0, N, BLOCK_M):
             i_end = min(i + BLOCK_M, N)
             Mi = i_end - i
-            Qi = Qf[:, :, i : i_end, :]  # (B, H, Mi, D)
-            Oi = torch.zeros((B, H, Mi, D), device=Q.device, dtype=torch.float32)
-            mi = torch.full((B, H, Mi), float("-inf"), device=Q.device, dtype=torch.float32)
-            li = torch.zeros((B, H, Mi), device=Q.device, dtype=torch.float32)
+            Qi = Qf[..., i : i_end, :]  # (..., Mi, D)
+            Oi = torch.zeros((*batch_dims, Mi, D), device=Q.device, dtype=torch.float32)
+            mi = torch.full((*batch_dims, Mi), float("-inf"), device=Q.device, dtype=torch.float32)
+            li = torch.zeros((*batch_dims, Mi), device=Q.device, dtype=torch.float32)
 
             for j in range(0, N, BLOCK_N):
                 # Nj = j_end - j
                 j_end = min(j + BLOCK_N, N)
-                Kj = Kf[:, :, j : j_end, :]  # (B, H, Nj, D)
-                Vj = Vf[:, :, j : j_end, :]  # (B, H, Nj, D)
+                Kj = Kf[..., j : j_end, :]  # (..., Nj, D)
+                Vj = Vf[..., j : j_end, :]  # (..., Nj, D)
 
-                Sij = einsum(Qi, Kj, "... q d, ... k d -> ... q k") * scale  # (B, H, Mi, Nj)
-                mij = torch.max(Sij, dim=-1).values  # (B, H, Mi)
-                m_new = torch.maximum(mi, mij)
-                alpha = torch.exp(mi - m_new)
-                Pij = torch.exp(Sij - m_new.unsqueeze(-1))  # (B, H, Mi, Nj)
+                Sij = einsum(Qi, Kj, "... q d, ... k d -> ... q k") * scale  # (..., Mi, Nj)
+                mij = torch.max(Sij, dim=-1).values  # (..., Mi)，这部分的最大值
+                m_new = torch.maximum(mi, mij)    # 更新该行的最大值
+                alpha = torch.exp(mi - m_new)    # 缩放系数
+                Pij = torch.exp(Sij - m_new.unsqueeze(-1))  # (..., Mi, Nj)
                 l_new = alpha * li + torch.sum(Pij, dim=-1)
 
                 Oi = alpha.unsqueeze(-1) * Oi + torch.matmul(Pij, Vj)
@@ -53,9 +53,9 @@ class FlashAttentionForwardPytorch(torch.autograd.Function):
                 li = l_new
 
             # 当前 query block 扫完所有 KV block 后，写回全局
-            O[:, :, i:i_end, :] = Oi / li.unsqueeze(-1)
-            m[:, :, i:i_end] = mi
-            l[:, :, i:i_end] = li
+            O[..., i:i_end, :] = Oi / li.unsqueeze(-1)
+            m[..., i:i_end] = mi
+            l[..., i:i_end] = li
 
         L = m + torch.log(l)
         O_out = O.to(Q.dtype)
--- assignment2-systems/tests/adapters.py
+++ assignment2-systems/tests/adapters.py
@@ -5,6 +5,7 @@ from typing import Type
 import torch
 
 
+from cs336_systems.flash_attention_pytorch import FlashAttentionForwardPytorch
 
 def get_flashattention_autograd_function_pytorch() -> Type:
     """
@@ -16,7 +17,7 @@ def get_flashattention_autograd_function_pytorch() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyFlashAttnAutogradFunctionClass
-    raise NotImplementedError
+    return FlashAttentionForwardPytorch
 
 
 def get_flashattention_autograd_function_triton() -> Type:
