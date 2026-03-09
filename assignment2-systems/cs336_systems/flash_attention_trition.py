3530ed088864e4f5a1857b8a4502630007b9097a
WalnuTpz <186885261+WalnuTpz@users.noreply.github.com>
2026-03-09 10:11:59 +0800

    完成主要计算流程

--- assignment2-systems/cs336_systems/flash_attention_trition.py
+++ assignment2-systems/cs336_systems/flash_attention_trition.py
@@ -26,9 +26,9 @@ def flash_fwd_kernel(
     query_tile_index = tl.program_id(0)
     batch_index = tl.program_id(1)
 
-    q_start = query_tile_index * Q_TILE_SIZE
-    offs_q = q_start + tl.arange(0, Q_TILE_SIZE)
-    valid_q = offs_q < N_QUERIES
+    q_start = query_tile_index * Q_TILE_SIZE    # 当前 q_tile 的起始 query 行号
+    offs_q = q_start + tl.arange(0, Q_TILE_SIZE)    # 当前 q_tile 中每一行的全局偏移量
+    valid_q = offs_q < N_QUERIES    # 合法的 query 行的位置
 
     Q_block_ptr = tl.make_block_ptr(
         base=Q_ptr + batch_index * stride_qb,
@@ -66,8 +66,44 @@ def flash_fwd_kernel(
         order=(1, 0),
     )
 
-    raise NotImplementedError
+    q = tl.load(Q_block_ptr, boundary_check=(0, 1), padding_option="zero").to(tl.float32)
+    m_i = tl.full((Q_TILE_SIZE,), float("-inf"), dtype=tl.float32)
+    l_i = tl.zeros((Q_TILE_SIZE,), dtype=tl.float32)
+    o_i = tl.zeros((Q_TILE_SIZE, D), dtype=tl.float32)
 
+    for k_start in range(0, N_KEYS, K_TILE_SIZE):
+        offs_k = k_start + tl.arange(0, K_TILE_SIZE)    # 当前 k_tile 中每一行的全局偏移量
+        valid_k = offs_k < N_KEYS    # 合法的 key 行的位置
+
+        k = tl.load(K_block_ptr, boundary_check=(0, 1), padding_option="zero")
+        v = tl.load(V_block_ptr, boundary_check=(0, 1), padding_option="zero")
+
+        # 分块 softmax
+        s_ij = tl.dot(q, k) * scale
+        valid = valid_q[:, None] & valid_k[None, :]  # (Q_TILE_SIZE, K_TILE_SIZE)
+        if IS_CAUSAL:
+            valid = valid & (offs_q[:, None] >= offs_k[None, :])
+        s_ij = tl.where(valid, s_ij, float("-inf"))
+
+        # 计算并更新最大值
+        m_ij = tl.max(s_ij, axis=1)
+        m_new = tl.maximum(m_i, m_ij)
+        safe_m_i = tl.where(valid_q, m_i, 0.0)    # 将非法位置的值改为 0，防止后续计算时出现 nan，后面也同理
+        safe_m_new = tl.where(valid_q, m_new, 0.0)
+
+        # 计算新的输出块
+        alpha = tl.where(valid_q, tl.exp(safe_m_i - safe_m_new), 0.0)
+        p_tilde = tl.where(valid, tl.exp(s_ij - safe_m_new[:, None]),0.0)
+        l_new = alpha * l_i + tl.sum(p_tilde, axis=1)
+        o_i = alpha[:, None] * o_i
+        o_i = tl.dot(p_tilde.to(v.dtype), v, acc=o_i)    # o_i = o_i + p_tilde @ v
+
+        m_i = m_new
+        l_i = l_new
+        K_block_ptr = tl.advance(K_block_ptr, (0, K_TILE_SIZE))
+        V_block_ptr = tl.advance(V_block_ptr, (K_TILE_SIZE, 0))
+
+    o_i = o_i / l_i[:, None]
 
 class FlashAttentionForwardTriton(torch.autograd.Function):
     @staticmethod
